5fdb84dee3269e207540dd7e72a580f874542f02
deuuid <deuuid@gmail.com>
2026-06-27 18:42:15 +0500

    neovim

---
+++ neovim/.config/nvim/init.lua
@@ -0,0 +1,73 @@
+vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+vim.cmd("colorscheme " .. "catppuccin")
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.keymap.set({'n', 'v'}, 'd', '"_d')
+vim.keymap.set({'n', 'v'}, 'c', '"_c')
+vim.keymap.set({'n', 'v'}, 'x', '"_x')
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.scrolloff = 10
+vim.opt.sidescrolloff = 8
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+-- vim.opt.colorcolumn = "90"
+vim.opt.showmatch = true
+
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "NONE", ctermbg = "NONE" })
+
+vim.pack.add({
+	{ src = "https://github.com/mason-org/mason.nvim", name = "mason" },
+})
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
+})
+
+vim.lsp.config.bashls = {
+  cmd = { 'bash-language-server', 'start' },
+  filetypes = { 'bash', 'sh' }
+}
+vim.lsp.enable 'bashls'
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "text",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "log",
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
+})
+
